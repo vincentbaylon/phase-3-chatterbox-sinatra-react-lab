30ca16a3eb5bae6364b7c1e91767ca1a4ee704fe
vincentbaylon <vincentbaylon@gmail.com>
2021-08-23 16:09:35 -0700

    commit tree b2d12750c9b87e51133f92a04c6e8f675a70912f

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,24 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    Message.all.to_json
+  end
   
+  post '/messages' do
+    Message.create(params).to_json
+  end
+
+  patch '/messages/:id' do
+    update_message = Message.find(params[:id])
+    update_message.update(params)
+    update_message.to_json
+  end
+
+  delete '/messages/:id' do
+    delete_message = Message.find(params[:id])
+    delete_message.destroy
+    delete_message.to_json
+  end
+
 end
